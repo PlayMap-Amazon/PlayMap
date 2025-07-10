ffc0e45d67782fa39c03390d134516fcf81b5994
ssandrasc <sasanz07@ucm.es>
2025-07-10 22:45:29 +0200

    springboot configuration

---
+++ PlayMap/src/main/java/com/project/aws/PlayMap/PlayMapApplication.java
@@ -0,0 +1,13 @@
+package com.project.aws.PlayMap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayMapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayMapApplication.class, args);
+	}
+
+}
---
+++ PlayMap/src/test/java/com/project/aws/PlayMap/PlayMapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.aws.PlayMap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayMapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
